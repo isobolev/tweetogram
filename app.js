90c1c4d999dff700e2273786248f1ea110fe1078
Bolgov Roman <bolgovr@gmail.com>
2012-09-30 00:10:45 +0300

    return values

--- app.js
+++ app.js
@@ -8,39 +8,40 @@ var everyauth = require('everyauth');
 var app = express();
 
 everyauth.twitter
-    .myHostname('http://tweetogram.clitika.com')
-    .consumerKey('Ib3kKgoKa5uFilCE4jTmcg')
-    .consumerSecret('sTGNnhiv6skQUveQF5bpkCnzJKW5dYpkm1674paQI')
-    .callbackPath('/auth/twitter/callback')
-    .findOrCreateUser(function (session, accessToken, accessTokenSecret, twitterUserMetadata) {
-        console.dir(arguments);
-    })
-    .redirectPath('/users');
+.myHostname('http://tweetogram.clitika.com')
+.consumerKey('Ib3kKgoKa5uFilCE4jTmcg')
+.consumerSecret('sTGNnhiv6skQUveQF5bpkCnzJKW5dYpkm1674paQI')
+.callbackPath('/auth/twitter/callback')
+.findOrCreateUser(function (session, accessToken, accessTokenSecret, twitterUserMetadata) {
+  console.dir(arguments);
+  return twitterUserMetadata;
+})
+.redirectPath('/users');
 
-app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
+app.configure(function () {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
 
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: 'waqasrfeAD' }));
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(everyauth.middleware());
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'waqasrfeAD' }));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(everyauth.middleware());
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-    app.use(express.errorHandler());
+app.configure('development', function () {
+  app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/users', routes.users);
 app.post('/generate', routes.generate);
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
 });
