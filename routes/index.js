c11a6f5d223cd1c021fe3947777648bc1b64a419
Bolgov Roman <bolgovr@gmail.com>
2012-09-30 10:05:48 +0300

    add instagram oauth

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var everyauth = require('everyauth');
 var request = require('request');
 var core = require('../core.js');
 exports.index = function (req, res) {
-  if (req.session && req.session.auth && req.session.auth.twitter) {
-    console.dir(req.session.auth.twitter);
-  }
+  console.dir(req.session.auth);
   res.render('index.ejs', {'session': req.session});
 };
 
